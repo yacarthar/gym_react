79213dc6c9c9b0e4d25b7c034d85daca83bec5fb
yacarthar <hoang.pham.vbhp@gmail.com>
2023-07-02 15:56:54 +0700

    raw display data, raw style

--- src/views/Home.js
+++ src/views/Home.js
@@ -10,14 +10,26 @@ const Home = () => {
       setProducts(res.data);
     };
     fetchData().catch(console.error);
-  });
+  }, []);
   return (
-    <div>
-      <ul>
+    <div className="container">
+      <div className="row mb-4">
         {products.map((p) => (
-          <li key={p.id}>{p.link}</li>
+          <div className="col-sm-6 col-md-3 col-lg-2">
+            <div className="card">
+              <img
+                src={p.images[0]}
+                alt="product-img"
+                className="img-fluid card-img-top"
+              />
+              <div className="card-body">
+                <h5 className="card-title">{p.title}</h5>
+                <p className="card-text">{p.price}</p>
+              </div>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
